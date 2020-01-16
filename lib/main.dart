dd801b56b4c2002724a86c30c8c1814917a0e594
Jaime Blasco <git@jaimeblasco.com>
2020-01-16 10:17:26 +0100

    :sparkles: New coming soon landpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:fluid_layout/fluid_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,38 +10,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Madrid',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        textTheme: TextTheme(
+            display3: GoogleFonts.balooBhai(
+                textStyle: textTheme.display3
+                    .copyWith(color: Color(0xff03569B), wordSpacing: 20)),
+            body1: GoogleFonts.montserrat(textStyle: textTheme.body1.copyWith(color: Colors.black.withOpacity(0.7)))),
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -59,53 +49,110 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return FluidLayout(
+
+        child: Scaffold(
+            body: Row(children: <Widget>[
+      Fluid(
+          child: Container(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+                Padding(
+                padding: EdgeInsets.only(left: 12),
+          child: Container(
+              padding: EdgeInsets.only(top: 72),
+              alignment: Alignment.topLeft,
+              child: Image.asset(
+                'assets/logo.png',
+                height: 50,
+              ),
+            ),),
+            Spacer(),
+            Text(' Hola! ', style: Theme.of(context).textTheme.display3),
+            SizedBox(height: 12),
+            Padding(
+              padding: EdgeInsets.only(left: 16),
+              child: Text(
+                  'Estamos preparando algo increible y emocionante\npara la comunidad de flutter en Madrid. \n\nNos vemos pronto!',
+                  style: Theme.of(context).textTheme.body1),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            SizedBox(height: 60),
+      Padding(
+        padding: EdgeInsets.only(left: 20),
+        child: Text('Únete y mantente informado:',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle
+                    .copyWith(color: Colors.black38)),),
+      Padding(
+        padding: EdgeInsets.only(left: 0),
+        child:Row(
+              children: <Widget>[
+                Padding(padding: EdgeInsets.all(8), child:IconButton(
+                    icon: Icon(FontAwesomeIcons.twitter),
+                    onPressed: () => launch(
+                        'https://twitter.com/FlutterMadrid'))),
+                Padding(padding: EdgeInsets.all(8), child:IconButton(
+                    icon: Icon(FontAwesomeIcons.telegram),
+                    onPressed: () => launch(
+                        'https://t.me/fluttermadrid'))),
+                Padding(padding: EdgeInsets.all(8), child: IconButton(
+                    icon: Icon(FontAwesomeIcons.meetup),
+                    onPressed: () =>
+                        launch('https://www.meetup.com/es-ES/Flutter-Madrid/'))),
+
+          Padding(padding: EdgeInsets.all(8), child: IconButton(
+                    icon: Icon(FontAwesomeIcons.github),
+                    onPressed: () => launch(
+                        'https://github.com/Flutter-Madrid/'))),
+              ],
+            ),),
+            SizedBox(height: 25),
+            Spacer(),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      )),
+      Expanded(child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 200)
+            return Container(
+              child: ClipPath(
+                  clipper: ArrowClipper(arrowDistance: 200),
+                  child: Stack(fit: StackFit.expand, children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/background.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    Container(
+                      color: Colors.black38,
+                    )
+                  ])),
+            );
+          else
+            return Container();
+        },
+      ))
+    ])));
   }
+}
+
+class ArrowClipper extends CustomClipper<Path> {
+  final arrowDistance;
+
+  ArrowClipper({this.arrowDistance = 12});
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(arrowDistance, size.height / 2);
+    path.lineTo(0, size.height);
+    path.lineTo(size.width, size.height);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }

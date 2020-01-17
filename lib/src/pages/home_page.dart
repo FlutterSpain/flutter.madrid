0a34092472c13e72f765c4c2e82d37a0b645b5c3
Alberto Chamorro <a.chamorro.ruiz@accenture.com>
2020-01-17 12:19:56 +0100

    :lipstick: Fixed UI problem on short devices and added Gmail link

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -14,63 +14,51 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return FluidLayout(
-      child: Scaffold(
-        body: Row(children: <Widget>[
-          Fluid(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: 12),
-                    child: Container(
-                      padding: EdgeInsets.only(top: 72),
-                      alignment: Alignment.topLeft,
-                      child: Image.asset(
-                        'assets/logo.png',
-                        height: 50,
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Text(' Hola! ', style: Theme.of(context).textTheme.display3),
-                  SizedBox(height: 12),
-                  Padding(
-                    padding: EdgeInsets.only(left: 16),
-                    child: Text(
-                        'Estamos preparando algo increible y emocionante\npara la comunidad de flutter en Madrid. \n\nNos vemos pronto!',
-                        style: Theme.of(context).textTheme.body1),
-                  ),
-                  SizedBox(height: 60),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20),
-                    child: Text('Únete y mantente informado:',
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle
-                            .copyWith(color: Colors.black38)),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(left: 0), child: _socialIconsRow()),
-                  SizedBox(height: 25),
-                  Spacer(),
-                ],
+        child: Scaffold(
+            body: Row(children: <Widget>[
+      Fluid(
+          child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(left: 12),
+              child: Container(
+                padding: EdgeInsets.only(top: 72),
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'assets/logo.png',
+                  height: 50,
+                ),
               ),
-            )
-          ),
-          Expanded(
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth > 50.0)
-                  return _backgroundImage(context);
-                else
-                  return Container();
-              },
-            )
-          )
-        ])
-      )
-    );
+            ),
+            Spacer(),
+            Text('Hola! ', style: Theme.of(context).textTheme.display3),
+            SizedBox(height: 12),
+            Text(
+                'Estamos preparando algo increíble\npara la comunidad de flutter en Madrid. \n\nNos vemos pronto!',
+                style: Theme.of(context).textTheme.body1),
+            SizedBox(height: 60),
+            Text('Únete y mantente informado:',
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle
+                    .copyWith(color: Colors.black38)),
+            _socialIconsRow(),
+            SizedBox(height: 25),
+            Spacer(),
+          ],
+        ),
+      )),
+      Expanded(child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 50.0)
+            return _backgroundImage(context);
+          else
+            return Container();
+        },
+      ))
+    ])));
   }
 }
 
@@ -78,25 +66,26 @@ Widget _backgroundImage(context) {
   return Container(
     child: ClipPath(
         clipper: ArrowClipper(arrowDistance: 200),
-        child: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/background.png'),
-              fit: BoxFit.cover,
-            ),
-            Container(
-              color: Colors.black38,
-            )
-          ]
-        )
-    ),
+        child: Stack(fit: StackFit.expand, children: <Widget>[
+          Image(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.cover,
+          ),
+          Container(
+            color: Colors.black38,
+          )
+        ])),
   );
 }
 
 Widget _socialIconsRow() {
   return Row(
     children: <Widget>[
+      Padding(
+          padding: EdgeInsets.all(8),
+          child: IconButton(
+              icon: Icon(FontAwesomeIcons.google),
+              onPressed: () => launch('mailto://fluttermadrid@gmail.com'))),
       Padding(
           padding: EdgeInsets.all(8),
           child: IconButton(

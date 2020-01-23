3f28e57ed7e54d3474290e9dfa8a867e6b9055c5
Jaime Blasco <git@jaimeblasco.com>
2020-01-23 16:32:42 +0100

    :sparkles: Add 10 feb meetup!

---
+++ lib/src/modules/meetups/meetup_model.dart
@@ -0,0 +1,10 @@
+
+
+class MeetupData {
+    final String title;
+    final String description;
+    final String imageURL;
+    final String meetUrl;
+
+  MeetupData({this.title, this.description, this.imageURL, this.meetUrl});
+}

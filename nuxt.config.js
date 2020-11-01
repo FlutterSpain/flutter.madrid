291a49737797155b1398cbf3facb053418ffe1e7
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2020-11-01 21:51:51 +0100

    :bug: Fixed linter errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,16 +5,34 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Comunidad de Flutter en Madrid' },
-      { name: "msapplication-TileColor", content: "#da532c" },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Comunidad de Flutter en Madrid',
+      },
+      { name: 'msapplication-TileColor', content: '#da532c' },
       { name: 'theme-color', content: '#ffffff'},
     ],
     link: [
-      { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
-      { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
-      { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
-      { rel: "manifest", href: "/site.webmanifest"},
-      { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5"},
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest'},
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5'},
     ],
   },
 

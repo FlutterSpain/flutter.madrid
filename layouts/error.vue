a569a88a2dcdbbdd9b2d0f2d1be85ef958364d45
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2020-11-03 06:57:53 +0100

    :bug: Fixed style on error pages

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="h-full">
-    <div class="flex flex-col items-center justify-center h-75">
+    <div class="flex flex-col items-center justify-center h-80">
       <h1 class="title mb-4 text-center dark:text-gray-main-200">
         Oops! Algo ha salido mal…
       </h1>
-      <ConfusedLight v-if="$colorMode.preference === 'light'" class="h-75" />
-      <ConfusedDark v-else class="h-75" />
+      <ConfusedLight
+        v-if="$colorMode.preference === 'light'"
+        class="confused-image w-4/5"
+      />
+      <ConfusedDark v-else class="confused-image w-4/5" />
       <Button text="Volver" class="my-5 lg:my-8" @click="goHome" />
     </div>
     <BaseFooter />
@@ -27,7 +30,23 @@ export default {
 </script>
 
 <style scoped>
-.h-75 {
-  height: 75%;
+.confused-image {
+  max-height: 18.75rem;
+}
+
+@media (min-width: 768px) {
+  .confused-image {
+    max-height: 31.25rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .confused-image {
+    max-height: 50rem;
+  }
+}
+
+.h-80 {
+  height: 80vh;
 }
 </style>
--- pages/404.vue
+++ pages/404.vue
@@ -3,15 +3,15 @@
     <BaseHeader />
     <main class="main overflow-y-auto bg-light dark:bg-dark">
       <div class="h-full">
-        <div class="flex flex-col items-center justify-center h-75">
+        <div class="flex flex-col items-center justify-center h-80">
           <h1 class="title mb-4 text-center dark:text-gray-main-200">
             Oops! Algo ha salido mal…
           </h1>
           <ConfusedLight
             v-if="$colorMode.preference === 'light'"
-            class="h-75"
+            class="confused-image"
           />
-          <ConfusedDark v-else class="h-75" />
+          <ConfusedDark v-else class="confused-image" />
           <Button text="Volver" class="my-5 lg:my-8" @click="goHome" />
         </div>
         <BaseFooter />
@@ -36,7 +36,24 @@ export default {
 </script>
 
 <style scoped>
-.h-75 {
-  height: 75%;
+.confused-image {
+  @apply w-4/5;
+  max-height: 18.75rem;
+}
+
+@media (min-width: 768px) {
+  .confused-image {
+    max-height: 31.25rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .confused-image {
+    max-height: 50rem;
+  }
+}
+
+.h-80 {
+  height: 80vh;
 }
 </style>
